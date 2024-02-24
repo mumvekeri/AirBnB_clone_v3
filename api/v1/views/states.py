01bcc7d6c2a4b71dcfaf0f8b8557a22c965c46c6
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-02-24 15:33:00 +0200

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,18 +3,15 @@
 from models.state import State
 from models import storage
 from api.v1.views import app_views
-from flask import abort, jsonify, make_response, request
-from flasgger.utils import swag_from
+from flask import abort, jsonify, request
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/state/get_state.yml', methods=['GET'])
 def get_states():
     """Retrieves the list of all State objects"""
     all_states = [state.to_dict() for state in storage.all(State).values()]
     return jsonify(all_states)
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/state/get_id_state.yml', methods=['GET'])
 def get_state(state_id):
     """Retrieves a specific State"""
     state = storage.get(State, state_id)
@@ -23,7 +20,6 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
-@swag_from('documentation/state/delete_state.yml', methods=['DELETE'])
 def delete_state(state_id):
     """Deletes a State Object"""
     state = storage.get(State, state_id)
@@ -31,10 +27,9 @@ def delete_state(state_id):
         abort(404)
     storage.delete(state)
     storage.save()
-    return make_response(jsonify({}), 200)
+    return jsonify({}), 200
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
-@swag_from('documentation/state/post_state.yml', methods=['POST'])
 def post_state():
     """Creates a State"""
     data = request.get_json()
@@ -44,10 +39,9 @@ def post_state():
         abort(400, description="Missing name")
     instance = State(**data)
     instance.save()
-    return make_response(jsonify(instance.to_dict()), 201)
+    return jsonify(instance.to_dict()), 201
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/state/put_state.yml', methods=['PUT'])
 def put_state(state_id):
     """Updates a State"""
     state = storage.get(State, state_id)
@@ -61,4 +55,4 @@ def put_state(state_id):
         if key not in ignore:
             setattr(state, key, value)
     storage.save()
-    return make_response(jsonify(state.to_dict()), 200)
+    return jsonify(state.to_dict()), 200
